a8ae63e788a8324dcd619f99c91f8c102c65eabd
sakuraghimire <sakuraghimire@localhost>
2024-06-20 20:45:05 -0400

    player name input implemented

--- src/ca/sheridancollege/WarGame/Main.java
+++ src/ca/sheridancollege/WarGame/Main.java
@@ -1,5 +1,7 @@
 package ca.sheridancollege.WarGame;
 
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,8 +14,13 @@ package ca.sheridancollege.WarGame;
 public class Main {
     public static void main(String[] args) {
         Games game = new Games("War");
-        Players player1 = new Players("Player 1");
-        Players player2 = new Players("Player 2");
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter player 1 Name: ");
+        Players player1 = new Players(in.nextLine());
+        System.out.print("Enter player 2 Name: ");
+        Players player2 = new Players(in.nextLine());
 
         game.addPlayer(player1);
         game.addPlayer(player2);
